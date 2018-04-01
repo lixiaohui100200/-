34362924434c9249ad77f768f426f9e0155958d0
lee <4856284@qq.com>
2018-04-02 00:54:51 +0800

    主页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,6 +9,11 @@ Page({
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
     ],
+    nav1:'/images/nav/1.png',
+    nav2: '/images/nav/2.png',
+    nav3: '/images/nav/3.png',
+    nav4: '/images/nav/4.png',
+    content:'/images/content/content.png',
     indicatorDots: true,
     autoplay: true,
     interval: 3000,
