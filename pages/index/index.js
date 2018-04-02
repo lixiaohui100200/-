29d29ae392409e28a7b5005e858af0da8757f929
lixiaohui100200 <4856284@qq.com>
2018-04-02 16:17:36 +0800

    '调整动态内容区'

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,21 +3,91 @@
 const app = getApp()
 
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
     imgUrls: [
       '/images/1.png',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
     ],
-    nav1:'/images/nav/1.png',
+    nav1: '/images/nav/1.png',
     nav2: '/images/nav/2.png',
     nav3: '/images/nav/3.png',
     nav4: '/images/nav/4.png',
-    content:'/images/content/content.png',
+    content: '/images/content/content.png',
     indicatorDots: true,
     autoplay: true,
     interval: 3000,
     duration: 1000,
-    circular:true
+    circular: true
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.getSystemInfo({
+      success: function (res) {
+        console.log(res.model)
+        console.log(res.pixelRatio)
+        console.log(res.windowWidth)
+        console.log(res.windowHeight)
+        console.log(res.language)
+        console.log(res.version)
+        console.log(res.platform)
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
   }
 })
